81c39384691b2c67b3310eaebc7d7a9ba6b60e23
Yaron Koresh <aharonkoresh1@gmail.com>
2024-02-12 14:51:01 +0200

    Add files via upload

--- out/asm2c.bat
+++ out/asm2c.bat
@@ -11,16 +11,27 @@ set "origin2=!dp!!name!.obj"
 set "origin3=!dp!!name!.exe"
 set "origin4=!dp!!name!.c"
 
-.\assembler\nasm.exe -f win64 -O1 --reproducible "!origin1!"
-.\linker\link.exe /entry:main /subsystem:windows /LARGEADDRESSAWARE:NO /fixed "!origin2!"
+(
+	.\assembler\nasm.exe -f win64 "!origin1!" 2>nul && .\linker\link.exe /entry:main /subsystem:windows /LARGEADDRESSAWARE:NO /fixed "!origin2!"
+) || (
+	del "!origin2!" 2>nul
+	.\assembler\nasm.exe -f win32 "!origin1!" 2>nul && .\linker\link.exe /entry:main /subsystem:windows /LARGEADDRESSAWARE:NO /fixed "!origin2!"
+) || (
+	del "!origin2!" 2>nul
+	.\assembler\nasm.exe -o "!origin2!" -f coff "!origin1!" 2>nul && .\linker\link.exe /entry:main /subsystem:windows /LARGEADDRESSAWARE:NO /fixed "!origin2!"
+) || (
+	echo Error
+	pause
+	exit /b 1
+)
 .\decompiler\bin\retdec-decompiler.exe -o "!origin4!" "!origin3!"
 
-del "!dp!!name!.config.json"
-del "!dp!!name!.bc"
-del "!dp!!name!.dsm"
-del "!dp!!name!.ll"
-del "!dp!!name!-unpacked"
-del "!origin2!"
-del "!origin3!"
+del "!dp!!name!.config.json" 2>nul
+del "!dp!!name!.bc" 2>nul
+del "!dp!!name!.dsm" 2>nul
+del "!dp!!name!.ll" 2>nul
+del "!dp!!name!-unpacked" 2>nul
+del "!origin2!" 2>nul
+del "!origin3!" 2>nul
 
 pause
